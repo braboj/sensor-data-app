befa77687603fc8771331349e796a59c3e7e88e8
Branimir Georgiev <braboj@gmail.com>
2025-01-25 13:30:38 +0200

    (#2) - Update the sensors interface and docstrings

--- backend/app/sensors.py
+++ backend/app/sensors.py
@@ -2,8 +2,8 @@ import random
 from abc import abstractmethod, ABC
 
 
-class SensorAbc(ABC):
-    """Abstract base class for sensors."""
+class _SensorAbc(ABC):
+    """Abstract class for sensors."""
 
     @property
     @abstractmethod
@@ -33,28 +33,11 @@ class SensorAbc(ABC):
         raise NotImplementedError
 
 
-class SensorBase(object):
+class _SensorBase(object):
     """Base class for field sensors handling."""
 
     def __init__(self, tag, low_limit, high_limit):
-        """Initialize the sensor with a tag name.
-
-        The low and high limits are used to generate random data within the
-        specified range for the sensor.
-
-        Example:
-
-            # Position sensor with 3 states (-1, 0, 1)
-            position = SensorBase('ZSS1R01A', low_limit=-1, high_limit=1)
-
-            # Temperature sensor with a range of 0 to 100 degC
-            temperature = SensorBase('TIS1P01A', low_limit=0, high_limit=100)
-
-        Args:
-            tag (str)                  : The tag name of the sensor.
-            low_limit (int | float)    : The low limit of the sensor.
-            high_limit (int | float)   : The high limit of the sensor.
-        """
+        """Initialize the sensor with a tag name."""
 
         self._tag = tag
         self._low_limit = low_limit
@@ -76,20 +59,64 @@ class SensorBase(object):
         return self._high_limit
 
 
-class DiscreteSensor(SensorBase, SensorAbc):
+class DiscreteSensor(_SensorBase, _SensorAbc):
     """Discete sensors to generate random binary data."""
+
+    def __init__(self, tag, low_limit=0, high_limit=1):
+        """Initialize the discrete sensor with a tag name.
+
+        Args:
+            tag (str)                  : The tag name of the sensor.
+            low_limit (int)            : The low limit of the sensor.
+            high_limit (int)           : The high limit of the sensor.
+
+        Example:
+            # Position sensor with 3 states (-1: left, 0: center, 1: right)
+            sensor = DiscreteSensor('ZSS1R01A', low_limit=-1, high_limit=1)
+
+            # Vibration sensor with 2 states (0: off, 1: on)
+            sensor = DiscreteSensor('VSS1R01A', low_limit=0, high_limit=1)
+        """
+
+        super(DiscreteSensor, self).__init__(tag, low_limit, high_limit)
 
     def read(self):
         """Read the discrete sensor data."""
         return random.randint(self.low_limit, self.high_limit)
 
 
-class AnalogSensor(SensorBase, SensorAbc):
-    """Analog sensors to generate random float data."""
+class AnalogSensor(_SensorBase, _SensorAbc):
+    """Analog sensors to generate random float data.
+
+    Args:
+        tag (str)                  : The tag name of the sensor.
+        low_limit (float)          : The low limit of the sensor.
+        high_limit (float)         : The high limit of the sensor.
+
+    Example:
+        # Temperature sensor with a range of 0-100 degrees
+        sensor = AnalogSensor('TIS1P01A', low_limit=0, high_limit=100)
+
+        # Pressure sensor with a range of 0-10 bar
+        sensor = AnalogSensor('PIS1P01A', low_limit=0, high_limit=10)
+    """
+
+    def __init__(self, tag, low_limit=0.0, high_limit=100.0):
+        """Initialize the analog sensor with a tag name.
+
+        Args:
+            tag (str)            : The tag name of the sensor.
+            low_limit  (float)   : The low limit of the sensor.
+            high_limit (float)   : The high limit of the sensor.
+        """
+        super(AnalogSensor, self).__init__(tag, low_limit, high_limit)
 
     def read(self):
         """Read the analog sensor data."""
         return random.uniform(self.low_limit, self.high_limit)
+
+
+__all__ = ['DiscreteSensor', 'AnalogSensor']
 
 
 def demo():
