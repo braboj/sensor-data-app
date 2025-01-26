fb359fb20351fb564ba3861d8afdcaaabffb8d6f
Branimir Georgiev <braboj@gmail.com>
2025-01-26 14:23:25 +0200

    (#4) - Add a nested component for the search results (part of the home view)

---
+++ frontend/tutorial/hello-world/src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  imports: [],
+  template: `
+    <p>
+      housing-location works!
+    </p>
+  `,
+  styles: ``
+})
+export class HousingLocationComponent {
+}
