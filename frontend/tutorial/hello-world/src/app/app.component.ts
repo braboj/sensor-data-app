78351272d577d72a8d5a068b1df570645c582850
Branimir Georgiev <braboj@gmail.com>
2025-01-26 16:08:08 +0200

    (#4) - Add a city filter

--- frontend/tutorial/hello-world/src/app/app.component.ts
+++ frontend/tutorial/hello-world/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {HomeComponent} from './home/home.component';
 import {RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
@@ -20,5 +19,4 @@ import {RouterLink, RouterOutlet} from '@angular/router';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'homes';
 }
