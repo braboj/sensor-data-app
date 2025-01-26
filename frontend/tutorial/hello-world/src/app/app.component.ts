4fe12cd2d8eb8df6034d106261d72fb09b390b50
Branimir Georgiev <braboj@gmail.com>
2025-01-26 12:49:14 +0200

    (#4) - Modify the hello world template as requested by the

--- frontend/tutorial/hello-world/src/app/app.component.ts
+++ frontend/tutorial/hello-world/src/app/app.component.ts
@@ -4,7 +4,7 @@ import {Component} from '@angular/core';
   selector: 'app-root',
   imports: [],
   template: `
-    <h1>Default</h1>
+    <h1>Hello by Branimir!</h1>
   `,
   styleUrls: ['./app.component.css'],
 })
