735ab44b591156393a88064984a598a41c611523
Branimir Georgiev <braboj@gmail.com>
2025-01-26 14:44:14 +0200

    (#4) - Add an input in the housing location component

--- frontend/tutorial/hello-world/src/app/housing-location/housing-location.component.ts
+++ frontend/tutorial/hello-world/src/app/housing-location/housing-location.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HousingLocation} from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
-  imports: [],
+  imports: [CommonModule],
   template: `
-    <p>
-      housing-location works!
-    </p>
+    <p>housing-location works!</p>
   `,
-  styles: ``
+  styleUrls: ['./housing-location.component.css'],
 })
 export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
 }
